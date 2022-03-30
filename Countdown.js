18f12dbef7e01dc17a8466a5de8abcf87ff7b2c7
Hassan Bazzi <Bazzih3519@gmail.com>
2022-03-30 01:30:46 -0400

    commit

---
+++ Countdown.js
@@ -0,0 +1,76 @@
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const certification = document.querySelector('.certification');
+  const deadline = document.querySelector('.deadline');
+  const items = document.querySelectorAll('.deadline-format h4');
+  
+  let tempDate = new Date(2022, 6, 6, 0);
+  let tempYear = tempDate.getFullYear();
+  let tempMonth = tempDate.getMonth();
+  let tempDay = tempDate.getDate();
+  const futureDate = new Date(tempYear, tempMonth, tempDay);
+  
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+  
+  let month = futureDate.getMonth();
+  month = months[month];
+  const weekday = weekdays[futureDate.getDay()];
+  const date = futureDate.getDate();
+  certification.textContent = `${weekday}, ${date} ${month} ${year}`;
+  
+  const futureTime = futureDate.getTime();
+  function getRemaindingTime() {
+    const today = new Date().getTime();
+  
+    const t = futureTime - today;
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+  
+    const values = [days, hours, minutes, seconds];
+    function format(item) {
+      if (item < 10) {
+        return (item = `0${item}`);
+      }
+      return item;
+    }
+  
+    items.forEach(function (item, index) {
+      item.innerHTML = format(values[index]);
+    });
+  
+    if (t < 0) {
+      clearInterval(countdown);
+      deadline.innerHTML = `<h4 class="expired">Congradulations you made it!</h4>`;
+    }
+  }
+  let countdown = setInterval(getRemaindingTime, 1000);
+  getRemaindingTime();
